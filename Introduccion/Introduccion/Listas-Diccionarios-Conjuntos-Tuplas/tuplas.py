7ce9d6cf90e9a93df8dbe102e3f45a53b77a0f65
dani17pl <daniel.perezl@um.es>
2018-09-17 23:46:24 +0200

    Tuplas

--- Introduccion/Introduccion/Listas-Diccionarios-Conjuntos-Tuplas/tuplas.py
+++ Introduccion/Introduccion/Listas-Diccionarios-Conjuntos-Tuplas/tuplas.py
@@ -3,6 +3,13 @@ Created on 17 sept. 2018
 
 @author: Dani_
 '''
-
+#Una tupla es como una lista inmutable, es decir, una secuencia ordenada que no puede cambiar.
+# Se suele emplear para manejar colecciones de ítems que son fijas, como los meses de un año,
 if __name__ == '__main__':
-    pass
+    N = ('do','re','mi','fa','sol','la','si')
+    #N[0] = ’ut’ # Lanza una excepción TypeError
+    #pueden crear tuplas usando la función tuple() que puede recibir como argumento una colección iterable (lista, conjunto u otra tupla).
+    c = ['zarangollo','spam','spam','paparajote']
+    print(c)
+    E=tuple(c)
+    print(E)
